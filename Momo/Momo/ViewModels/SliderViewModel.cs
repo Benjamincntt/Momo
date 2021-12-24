80261bdf022be93ad48c9575d6204399366fc022
luonghieu nguyen <nguyenluonghieupro@gmail.com>
2021-12-24 09:22:33 +0700

    22/12

---
+++ Momo/Momo/ViewModels/SliderViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Momo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms.Internals;
+
+namespace Momo.ViewModels
+{
+    public interface ISlideViewModel { }
+    public class SliderViewModel:BaseViewModel,ISlideViewModel
+    {
+        private ObservableCollection<SliderModel> _slider;
+        public ObservableCollection<SliderModel> Sliders
+        {
+            get { return _slider; }
+            set { _slider = value; OnPropertyChanged(nameof(Sliders)); }
+        }
+        [Preserve]
+        public SliderViewModel()
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            var sliders = new List<SliderModel>
+            {
+                new SliderModel{ID=1,Uri="https://cafebiz.cafebizcdn.vn/thumb_w/600/2018/9/12/photo1536760206584-15367602065851855802683.jpg" },
+                new SliderModel{ID=2,Uri="https://cafebiz.cafebizcdn.vn/thumb_w/600/2018/9/12/photo1536760206584-15367602065851855802683.jpg" }
+            };
+            Sliders = new ObservableCollection<SliderModel>(sliders);
+        }
+    }
+}
---
+++ Momo/Momo/Views/SliderView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Momo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SliderView : ContentPage
+    {
+        public SliderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
